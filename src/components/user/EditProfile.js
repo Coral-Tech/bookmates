e0d2b682ab71f84ec199d4b583de4ef0b36c37e9
smonteroparis <smonteroparis@icloud.com>
2020-05-05 15:31:52 +0100

    added edit profile component skeleton

---
+++ src/components/user/EditProfile.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+// -------------------------- TO DO  --------------------------
+// * All
+// * Design
+// ------------------------------------------------------------
+
+class EditProfile extends Component {
+  render() {
+    return (
+      <View>
+        <Text>EditProfile</Text>
+      </View>
+    );
+  }
+}
+
+export default EditProfile;
